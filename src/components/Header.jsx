fb1f9b2977105982fd868b6bb2b659b2e8580072
James <jnr1298@aol.com>
2023-07-11 13:46:48 -0400

    Initial redesign commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import { FiMenu, FiX } from "react-icons/fi";
+
+export default function Header() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+      setIsMenuOpen(!isMenuOpen);
+    };
+    return (
+        <nav className="flex items-center justify-between bg-gray-800 py-4 pt-8 mb-20">
+            <div className="flex items-center">
+                <a href="/" className="text-black text-lg font-bold">
+                    James Darrigo
+                </a>
+                </div>
+                <div className="flex items-center">
+                <div className="hidden md:block">
+                    {/* Render navigation links for larger screens */}
+                    <a href="/" className="text-black px-4">
+                    Home
+                    </a>
+                    <a href="/" className="text-black px-4">
+                    About
+                    </a>
+                    <a href="/" className="text-black px-4">
+                    Services
+                    </a>
+                    <a href="/" className="text-black px-4">
+                    Contact
+                    </a>
+                </div>
+                <div className="md:hidden">
+                    {/* Render burger menu icon for smaller screens */}
+                    <button
+                    onClick={toggleMenu}
+                    className="text-black focus:outline-none"
+                    >
+                    {isMenuOpen ? (
+                        <FiX className="text-2xl" />
+                    ) : (
+                        <FiMenu className="text-2xl" />
+                    )}
+                    </button>
+                </div>
+            </div>
+            {isMenuOpen && (
+                <div className="md:hidden">
+                    {/* Render dropdown menu for smaller screens */}
+                    <div className="bg-dark py-2 px-4">
+                    <a href="/" className="block text-black py-2">
+                        Home
+                    </a>
+                    <a href="/" className="block text-black py-2">
+                        About
+                    </a>
+                    <a href="/" className="block text-black py-2">
+                        Services
+                    </a>
+                    <a href="/" className="block text-black py-2">
+                        Contact
+                    </a>
+                    </div>
+                </div>
+            )}
+        </nav>
+    );
+  }
---
+++ src/components/Layout.jsx
@@ -0,0 +1,25 @@
+export default function Main() {
+    return (
+      <div>
+        <section className="w-full text-black">
+          <div className=" inline-block items-center pt-0 lg:flex lg:flex-wrap lg:pt-4">
+            <div className="lg:w-3/6">
+              <h2 className="max-w-xl lg:text-[4.2em] text-3xl font-bold leading-none text-black inline-block">
+                Hi! 👋 My name is James
+              </h2>
+  
+              <p className="mt-6 max-w-2xl text-xl font-semibold text-[#404040]">
+                I'm a software developer experienced in architecting & building modern, scalable business solutions
+              </p>
+            </div>
+            <div className="mb-20 mt-44 hidden w-full flex-col lg:mt-12 lg:inline-block lg:w-3/6">
+              <img src="/images/placeholder.png" alt="Hero" />
+            </div>
+            <div className="my-20 inline-block w-full flex-col lg:mt-0 lg:hidden lg:w-2/5">
+              <img src="/images/placeholder.png" alt="image" />
+            </div>
+          </div>
+        </section>
+      </div>
+    );
+  }
